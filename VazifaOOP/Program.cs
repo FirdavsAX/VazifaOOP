298cdb0610b17e297c9bdc083d9827c779bcbf44
FirdavsAX <firdavsjumayev320@gmail.com>
2023-08-08 09:59:33 +0500

    Twoo tasks in one sln File

--- VazifaOOP/Program.cs
+++ VazifaOOP/Program.cs
@@ -11,6 +11,17 @@
             a.SetHeight(12.2);
             a.SetWidth(10);
             a.Calculate();
+           
+
+            Student student1 = new Student();
+
+            student1.SetName("Ronald Tolkien");
+            student1.SetRollNumber(12345);
+            student1.SetMarks(2,3,4,4,4,5);
+            
+            student1.CalculateTotalPoint();
+            
+            Console.ReadKey();
         }
     }
     class Rectangle
@@ -44,5 +55,44 @@
         }
         
 
+    }
+    class Student
+    {
+        public string Name { get; private set; }
+        public int RollNumber { get; private set; }
+        public int[] Marks { get; private set; }
+
+        public void SetName(string fullName)
+        {
+            if (string.IsNullOrEmpty(fullName))
+            {
+                Console.WriteLine("Name is empty");
+                return;
+            }
+        }
+        public void SetRollNumber(int rollNumber)
+        {
+            if(rollNumber <=0)
+            {
+                Console.WriteLine("Incorrect form!");
+                return;
+            }
+            RollNumber = rollNumber;
+
+        }
+        public void SetMarks(params int[] marks)
+        {
+            if (marks.Length == 0)
+            {
+                Console.WriteLine("Marks are empty !");
+                return;
+            }
+            Marks = marks;
+        }
+
+       public void CalculateTotalPoint()
+        {
+            Console.WriteLine("Your overage point : " + Marks.Sum() / Marks.Length);
+        }
     }
 }
